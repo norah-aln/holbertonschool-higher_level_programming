41addc159d0f9a85cb63fb1cd146d01695d1bcb1
norah-aln <zylayo@gmail.com>
2026-01-17 19:31:27 +0000

    Add Python Object Relational Mapping tasks

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Displays all values in states table where name matches the argument"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,37 @@
+
+# Python - Object-relational mapping
+
+This project demonstrates connecting Python with MySQL databases using MySQLdb and SQLAlchemy ORM.
+
+## Tasks
+
+**MySQLdb (Tasks 0-5)**
+
+- Connecting to MySQL from Python
+
+- Executing SQL queries
+
+- Handling SQL injection
+
+**SQLAlchemy ORM (Tasks 6-14)**
+
+- Defining models
+
+- CRUD operations
+
+- Relationships between tables
+
+## Requirements
+
+- Python 3.8.5
+
+- MySQLdb 2.0.3
+
+- SQLAlchemy 1.4.22
+
+- MySQL 8.0
+
+## Author
+
+Norah Alnujidi
+
